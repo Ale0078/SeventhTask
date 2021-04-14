b37dacc46b6b79d4d73fb0e7ef5bdaf5213d469b
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-14 22:29:25 +0100

    SeventhTask.Logic was changed .Class NaturalSequenceBuilder was added to the path Components/Builders

---
+++ SeventhTask.Logic/Components/Builders/NaturalSequenceBuilder.cs
@@ -0,0 +1,17 @@
+﻿using SeventhTask.Logic.Components.Interfaces;
+using SeventhTask.Logic.Components.Builders.Abstracts;
+
+namespace SeventhTask.Logic.Components.Builders
+{
+    public class NaturalSequenceBuilder : SequenceBuilder
+    {
+        public NaturalSequenceBuilder(double maxRange) : base(maxRange) 
+        {
+        }
+
+        public override ISequence Create()
+        {
+            return new NaturalSequence(MaxRange);
+        }
+    }
+}
