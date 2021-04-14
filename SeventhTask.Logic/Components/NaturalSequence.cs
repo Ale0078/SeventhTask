cd7094112bc7bf2b60ea9c9d6f363ea833c0454e
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-14 22:25:55 +0100

    SeventhTask.Logic was changed .Class NaturalSequence was added to the path Components

---
+++ SeventhTask.Logic/Components/NaturalSequence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using SeventhTask.Logic.Components.Interfaces;
+
+namespace SeventhTask.Logic.Components
+{
+    internal class NaturalSequence : ISequence
+    {
+        public const int POW_TO_NATURAL_NUMBER = 2;
+
+        public double MaxRange { get; set; }
+
+        public NaturalSequence(double maxRange) 
+        {
+            MaxRange = maxRange;
+        }
+
+        public IEnumerable<int> GetEnumerator() 
+        {
+            for (int i = 0; Math.Pow(i, POW_TO_NATURAL_NUMBER) < MaxRange; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
