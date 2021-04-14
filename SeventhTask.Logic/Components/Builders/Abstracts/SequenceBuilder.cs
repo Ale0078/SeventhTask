631c009558c47152960f192afe0b44b03151f83b
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-14 22:27:46 +0100

    SeventhTask.Logic was changed .Abstract Class SequenceBuilder was added to the path Components/Builders/Abstracts

---
+++ SeventhTask.Logic/Components/Builders/Abstracts/SequenceBuilder.cs
@@ -0,0 +1,16 @@
+﻿using SeventhTask.Logic.Components.Interfaces;
+
+namespace SeventhTask.Logic.Components.Builders.Abstracts
+{
+    public abstract class SequenceBuilder
+    {
+        public double MaxRange { get; }
+
+        public SequenceBuilder(double maxRange) 
+        {
+            MaxRange = maxRange;
+        }
+
+        public abstract ISequence Create();
+    }
+}
