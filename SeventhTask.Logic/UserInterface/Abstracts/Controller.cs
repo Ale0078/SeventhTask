cf179e01875d8fc0a6554ac953b9045d4e3a3a12
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-14 22:34:43 +0100

    SeventhTask.Logic was changed .Abstratc Class Controller was added to the path UserInterface/Abstracts

---
+++ SeventhTask.Logic/UserInterface/Abstracts/Controller.cs
@@ -0,0 +1,17 @@
+﻿using SeventhTask.Logic.Components.Builders.Abstracts;
+
+namespace SeventhTask.Logic.UserInterface.Abstracts
+{
+    public abstract class Controller
+    {
+        public SequenceBuilder SequenceCreater { get; protected set; }
+
+        public Controller(SequenceBuilder sequenceCreater) 
+        {
+            SequenceCreater = sequenceCreater;
+        }
+
+        public abstract void Display();
+        public abstract void SetSequenceCreater(SequenceBuilder sequenceCreater);
+    }
+}
